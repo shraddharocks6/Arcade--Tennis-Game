b82d85c898e07ec200dfcc2d17799b8057974e11
dsfksh <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2023-07-09 03:21:07 +0530

    added pygame for local serve and extensibility

--- tennisgame/tennis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'tennis'
-
-urlpatterns = [
-    path('', views.tennis_game, name='tennis_game'),
-]
--- tennisgame/tennis/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def tennis_game(request):
-    return render(request, 'tennis/game.html')
